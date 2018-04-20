3a8170c58672d60ee3af2dd70e988c505e115f0d
Lee Nowotny <lee.nowotny@ni.com>
2018-04-20 13:59:37 -0500

    We now save messages to local storage

--- js/messageLogic.js
+++ js/messageLogic.js
@@ -1,10 +1,26 @@
-function sendMessage()
-{
-}
-
 function initializeSendMessageForm(sendMessageForm)
 {
-    debugger;
     var userName = getUserName();
     document.getElementById("from").value = userName;
+}
+
+function sendMessage()
+{
+    var message = {};
+    message.to = document.getElementById("to").value;
+    message.address = document.getElementById("address").value;
+    message.content = document.getElementById("subject").value;
+    message.timestamp = Date.now();
+
+    var messages = [];
+    var previousMessagesJson = localStorage.getItem(from);
+    if (previousMessagesJson)
+    {
+        messages = JSON.parse(previousMessagesJson);
+    }
+
+    messages.push(message);
+
+    var messagesString = JSON.stringify(messages);
+    localStorage.setItem(from, messagesString);
 }
